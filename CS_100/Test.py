85c083c6cc148fc8dee61f635642d225e96b2b84
ArnavKucheriya <ArnavKucheriya@outlook.com>
2023-09-09 20:29:03 -0400

    Adding Array and various operations on the array.

--- CS_100/Test.py
+++ CS_100/Test.py
@@ -10,9 +10,23 @@ print('Value of a + b = ', a + b)
 a = 'Changing the value of a to a String'
 print(a)
 
+print('Rolling a Dice!')
 # Random Number Generator using Numpy (Pip installing numpy)
 print(np.random.randint(1,9))
 
 msg = 'hello world'
 print(msg.capitalize())
 print(msg.split('e'))
+
+# Adding a orray to the program
+array = np.array([1,2,3,4,5,6,7,8,9,10])
+
+#Printing the array
+print(array)
+
+#Performing different operations on the array
+print(array[0])
+print(array[1:5])
+print(array[5:])
+print(array[:5])
+print(array[-1])
