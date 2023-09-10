06038081f0a1de43e3df68e6d1710d57b184c77d
ArnavKucheriya <ArnavKucheriya@outlook.com>
2023-09-09 21:14:27 -0400

    True or False with 2d Arrays.

--- CS_100/Test.py
+++ CS_100/Test.py
@@ -100,6 +100,23 @@ arr = [[0 for i in range(cols)] for j in range(rows)]
 arr[0][0] = 1
 for row in arr:
 	print(row)
+print()
+
+rows, cols = (5, 5)
+
+# method 2 2nd approach
+arr = [[0 for i in range(cols)] for j in range(rows)]
+
+# check if arr[0] and arr[1] refer to
+# the same object
+print(arr[0] is arr[1]) # prints False
+
+# method 2 1st approach
+arr = [[0]*cols]*rows
+
+# check if arr[0] and arr[1] refer to the same object prints True because there is only one
+#list object being created.
+print(arr[0] is arr[1])
 
 
    
