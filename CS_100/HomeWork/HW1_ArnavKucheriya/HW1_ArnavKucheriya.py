a449931d7c630fe6a7df722f479a78a71c06cffd
ArnavKucheriya <ArnavKucheriya@outlook.com>
2023-09-11 13:35:18 -0400

    Adding Homework Folder, Chapter 1-2 and Homework1

---
+++ CS_100/HomeWork/HW1_ArnavKucheriya/HW1_ArnavKucheriya.py
@@ -0,0 +1,28 @@
+'''
+Arnav Kucheriya
+CS 100 015
+HW 01, September 11, 2023
+'''
+
+#Exercise 5b
+int1 = 12
+int2 = 3
+int3 = 5
+
+#Exercise 5c
+float1_Terminating = 12.0
+float2_Recurring = 3.333333
+float3_Pi = 3.141592653589793238462643383279502884197169399375105820974944592307816406286
+
+#Exercise 5d
+string1 = "Hello World!"
+string2 = "This is assignment 1 - Homework 1"
+string3 = "I am a student at New Jersey Institute of Technology"
+
+#Exercise 6 [1.1]
+
+#Exercise 6 [1.2]
+
+#Exercise 6 [2.1]
+
+#Exercise 6 [2.2]
