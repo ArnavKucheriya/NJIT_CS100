3bbdf23da1ff98a9249daa7fe931404f2a4335ed
ArnavKucheriya <ArnavKucheriya@outlook.com>
2023-09-11 13:57:16 -0400

    Adding HomeWork Questions (Unanswered)

--- CS_100/HomeWork/HW1_ArnavKucheriya/HW1_ArnavKucheriya.py
+++ CS_100/HomeWork/HW1_ArnavKucheriya/HW1_ArnavKucheriya.py
@@ -19,10 +19,58 @@ string1 = "Hello World!"
 string2 = "This is assignment 1 - Homework 1"
 string3 = "I am a student at New Jersey Institute of Technology"
 
+'''
+====================================================================================================
 #Exercise 6 [1.1]
 
+Q1. In a print statement, what happens if you leave out one of the parentheses, or both?
+A1. 
+
+Q2. If you are trying to print a string, what happens if you leave out one of the quotation marks, or both?
+A2.
+
+Q3. You can use a minus sign to make a negative number like -2. What happens if you put a plus sign before a number? What about 2++2?
+A3.
+
+====================================================================================================
 #Exercise 6 [1.2]
 
+Q1. How many seconds are there in 42 minutes 42 seconds?
+A1.
+
+Q2. How many miles are there in 10 kilometers? Hint: there are 1.61 kilometers in a mile.
+A2.
+
+Q3. If you un a 10 kilometer race in 42 minutes 42 seconds, what is your average pace (time per
+mile in minutes and seconds)? What is your average speed in miles per hour?
+A3.
+
+====================================================================================================
 #Exercise 6 [2.1]
 
+Q.Repeating my advice from the previous chapter, whenever you learn a new feature,
+you should try it out in interactive mode and make errors on purpose to see what goes wrong.
+• We've seen that n = 42 is legal. What about 42 = n?
+• How about x = y = 1?
+• In some languages every statement ends with a semi-colon, ;. What happens if you put a
+semi-colon at the end of a Python statement?
+• What if you put a period at the end of a statement?
+• In math notation you can multiply x and y like this: xy. What happens if you try that in
+Python? 
+
+A. 
+
+====================================================================================================
 #Exercise 6 [2.2]
+
+Q1. The volume of a sphere with radius r is 4/3 πr3. What is the volume of a sphere with radius 5?
+A1. 
+
+Q2. Suppose the cover price of a book is $24.95, but bookstores get a 40% discount. Shipping costs
+A2.
+
+Q3. If I leave my house at 6:52 am and run 1 mile at an easy pace (8:15 per mile), then 3 miles
+A3.
+
+====================================================================================================
+'''
