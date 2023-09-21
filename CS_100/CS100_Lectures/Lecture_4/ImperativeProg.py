e60cd2a4caad2e7e40750c41ef6c45db99ebfbfd
ArnavKucheriya <ArnavKucheriya@outlook.com>
2023-09-21 13:45:20 -0400

    Programming using Turtle Graphics

---
+++ CS_100/CS100_Lectures/Lecture_4/ImperativeProg.py
@@ -0,0 +1,10 @@
+line1 = 'Hello Python developer...'
+line2 = 'Welcome to the world of Python!'
+print(line1)
+print(line2)
+print('hello', end='!!!')
+print()
+name = input('Enter your name: ')
+print(name)
+#print('Your First name is:', name[0])
+#print('Your Last name is:', name[-1])
