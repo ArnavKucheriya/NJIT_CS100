d768adb6213a71edae43dcfdf4d083f870a2f266
ArnavKucheriya <ArnavKucheriya@outlook.com>
2023-09-21 13:59:34 -0400

    Imperitive statements in Python

--- CS_100/CS100_Lectures/Lecture_4/ImperativeProg.py
+++ CS_100/CS100_Lectures/Lecture_4/ImperativeProg.py
@@ -2,9 +2,15 @@ line1 = 'Hello Python developer...'
 line2 = 'Welcome to the world of Python!'
 print(line1)
 print(line2)
-print('hello', end='!!!')
-print()
-name = input('Enter your name: ')
-print(name)
+print('hello', end='!!!--')
+print('No new Line', '\n')
+
+first_Name = input('Enter your first name: ')
+last_Name = input('Enter your last name: ')
+age = int(input('Enter your age: '))
+print('Name:',first_Name, last_Name)
+print('Age:',age)
+
+
 #print('Your First name is:', name[0])
 #print('Your Last name is:', name[-1])
