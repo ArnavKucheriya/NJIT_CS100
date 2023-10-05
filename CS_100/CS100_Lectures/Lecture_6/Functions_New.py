4c3bc6be6dcfee3c357da1268fbb5a5e9607ae42
ArnavKucheriya <ArnavKucheriya@outlook.com>
2023-10-05 13:16:30 -0400

    Adding a new Lecture_6 file: Functions_New

---
+++ CS_100/CS100_Lectures/Lecture_6/Functions_New.py
@@ -0,0 +1,7 @@
+# New file for Lecture 6: Functions (Defining, Calling, and Returning)
+
+def average_Value_2_Numbers(val1, val2):
+    return (val1 + val2) / 2
+print(average_Value_2_Numbers(2, 4))
+print()
+
