a7c9028b4c279efda50e2528160ac14c08119ed8
ArnavKucheriya <ArnavKucheriya@outlook.com>
2023-11-27 13:11:22 -0500

    Working on Lecture_12.py

---
+++ CS_100/CS100_Lectures/Lecture_12/Namespace.py
@@ -0,0 +1,12 @@
+'''
+Namespaces - Local and Global
+- The pupose of Functions
+- Global versus Local Namespaces
+- The Program Stack
+- Python's Name Evaluation Rules
+
+The purpose of functions
+- Modularity: They allow us to break down complex problems that require complex programs into small, simple, self-contained pieces. Each small piece can be implemented, tested, and debugged independently.
+- Abstraction: A function has a name that should clearly reflects what it does. That action can then be performed by calling the function by name, abstracting what the function does from how it does it.
+- Reusability: Code that may be used multiple times should be packaged in a function. The code only needs to be written once. And any time that it needs to be modified, extended or debugged, the changes need to be made only once. 
+'''
